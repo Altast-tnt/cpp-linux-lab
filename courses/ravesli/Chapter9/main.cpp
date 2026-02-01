108c9a855716ed9572ce4e8fc7e832f44d8f9b74
Angelina Zamataeva <altast-t@ya.ru>
2026-02-01 16:34:55 +0300

    Chapter9 перегрузка унарных операторов

--- courses/ravesli/Chapter9/main.cpp
+++ courses/ravesli/Chapter9/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include "Fraction.h"
+#include "Something.h"
 
 int main()
 {
-	Fraction f1;
-	std::cout << "Enter fraction 1: ";
-	std::cin >> f1;
+    Something something;
 
-	Fraction f2;
-	std::cout << "Enter fraction 2: ";
-	std::cin >> f2;
+	if (!something)
+		std::cout << "Something is null.\n";
+	else
+		std::cout << "Something is not null.\n";
 
-	std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n';
+//	Fraction f1;
+//	std::cout << "Enter fraction 1: ";
+//	std::cin >> f1;
+//
+//	Fraction f2;
+//	std::cout << "Enter fraction 2: ";
+//	std::cin >> f2;
+//
+//	std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n';
     return 0;
 }
