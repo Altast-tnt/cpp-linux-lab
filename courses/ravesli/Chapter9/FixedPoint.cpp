87d014d54d2c90462eed949a81d17ef6dd0e85c4
Angelina Zamataeva <altast-t@ya.ru>
2026-02-03 12:07:17 +0300

    Chapter9 final test finish

--- courses/ravesli/Chapter9/FixedPoint.cpp
+++ courses/ravesli/Chapter9/FixedPoint.cpp
@@ -2,22 +2,9 @@
 #include <iostream>
 #include <cmath>
 
-FixedPoint::FixedPoint(int16_t val, int8_t frac)
+FixedPoint::FixedPoint(int16_t val, int8_t frac) : m_val(val), m_frac(frac)
 {
-
-    if (val > 0 && frac < 0)
-    {
-        m_val = -val;
-        m_frac = frac;
-    } else if (val < 0 && frac > 0)
-    {
-        m_val = val;
-        m_frac = -frac;
-    } else
-    {
-        m_val = val;
-        m_frac = frac;
-    }
+    if (m_val < 0 && m_frac > 0) m_frac = -m_frac;
 }
 
 FixedPoint::FixedPoint(double num)
@@ -26,15 +13,38 @@ FixedPoint::FixedPoint(double num)
     m_frac = static_cast<int8_t>(std::round((num - m_val) * 100));
 }
 
-std::ostream& operator<<(std::ostream &out, FixedPoint &fi)
+std::ostream& operator<<(std::ostream &out, const FixedPoint &fi)
 {
 
     out << static_cast<double>(fi);
     return out;
 }
 
+FixedPoint operator+(const FixedPoint &f1, const FixedPoint &f2)
+{
+    return FixedPoint(static_cast<double>(f1) + static_cast<double>(f2));
+}
+
 
 FixedPoint::operator double() const
 {
     return static_cast<double>(m_val) + (static_cast<double>(m_frac) / 100.0);
+}
+
+FixedPoint FixedPoint::operator-()
+{
+    return FixedPoint(
+        static_cast<int16_t>(-m_val),
+        static_cast<int8_t>(-m_frac)
+    );
+}
+
+std::istream& operator>>(std::istream &in, FixedPoint &fi)
+{
+    double d;
+    if (in >> d)
+    {
+        fi = FixedPoint(d);
+    }
+    return in;
 }
--- courses/ravesli/Chapter9/FixedPoint.h
+++ courses/ravesli/Chapter9/FixedPoint.h
@@ -10,9 +10,14 @@ private:
 public:
     FixedPoint(int16_t val, int8_t frac);
     FixedPoint(double num);
+
     operator double() const;
+    FixedPoint operator-();
+
+    friend FixedPoint operator+ (const FixedPoint &f1, const FixedPoint &f2);
+    friend std::ostream& operator<<(std::ostream &out, const FixedPoint &fi);
+    friend std::istream& operator>>(std::istream &in, FixedPoint &fi);
 
 
-    friend std::ostream& operator<<(std::ostream &out, FixedPoint &fi);
 
 };
