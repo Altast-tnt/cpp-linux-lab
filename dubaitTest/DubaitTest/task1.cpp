a53d2214b9e3655429cd9cab4430a238489c4c9c
Angelina Zamataeva <altast-t@ya.ru>
2026-03-15 22:06:44 +0300

    /

--- dubaitTest/DubaitTest/task1.cpp
+++ dubaitTest/DubaitTest/task1.cpp
@@ -1,49 +1,72 @@
 #include <iostream>
 
+
+
+
 unsigned long long  task1()
 {
-    unsigned long long counts[100] {0};
+    unsigned long long counts[28] {0};
 
     // Из условия задачи
     counts[0] = 1; // F[1] = 1
     counts[1] = 2; // F[2] = 2
 
     int maxNum = 100000;
+    // до 100000
+    int iter = 1;
+    // для сумм - будет считаться в обратном порядке от основного итератора
+    int sumIter = iter;
 
-    while (maxNum != 0)
+
+    while (iter != maxNum)
     {
         // попробовать через указатели двигаться пачками по 26
         // рассчет сумм
-        for (int i = 0; i < 27; ++i)
+        for (int i = 0; i < 28; ++i)
         {
             // рассчет для нечетного числа
-            if ((i+1) % 2 != 0)
+            // исключение самого первого числа из рассчета
+            if ((iter % 2 != 0) & (iter != 1))
             {
                 for (int j = i; j >= 0; --j)
                 {
-                    if ((j+1) % 2 == 0)
+                    if (sumIter % 2 == 0)
                     {
                         counts[i] += counts[j];
                     }
+                    --sumIter;
                 }
             }
 
             // рассчет для четного числа
-            if ((i+1) % 2 == 0)
+            // исключение второго числа из рассчета
+            if ((iter % 2 == 0) & (iter != 2))
             {
                 for (int j = i; j >= 0; --j)
                 {
-                    if ((j+1) % 2 != 0)
+                    if (sumIter % 2 != 0)
                     {
                         counts[i] += counts[j];
                     }
+                    // двигаемся назад по списку чисел
+                    --sumIter;
                 }
             }
-        }
-        maxNum -= 100;
-        // для двух чисел нужен подсчет отдельно
 
+            if (iter == maxNum)
+            {
+
+                break;
+            }
+            // переход к следующему числу
+            ++iter;
+            sumIter = iter;
+        }
     }
+
+
+
+
 
 // нужно чтобы в цикле было 13 чисел, из 25 получается по 12
 // либо внутри цикла менять количество условий и итераций
@@ -51,10 +74,14 @@ unsigned long long  task1()
 
     // вывод для проверки
     std::cout << "ПРОВЕРКА\n";
-    for (int i = 0; i < 26; ++i)
+    for (int i = 0; i < 28; ++i)
     {
         std::cout << counts[i] << "\n";
     }
+
+    std::cout << iter << "\n";
+    std::cout << counts[27] << "\n";
+    std::cout << counts[28] << "\n";
 
     return 1;
 }
