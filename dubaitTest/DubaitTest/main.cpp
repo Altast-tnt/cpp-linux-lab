653e4ad872d16800fddae1efd8e102b47d09cd1a
Angelina Zamataeva <altast-t@ya.ru>
2026-03-16 17:04:43 +0300

    task1 complete

--- dubaitTest/DubaitTest/main.cpp
+++ dubaitTest/DubaitTest/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 unsigned long long test();
-unsigned long long  task1();
+int  task1();
 
 int main()
 {
 
     //unsigned long long finalAns = test();
-    task1();
+    std::cout << task1();
 
     //std::cout << "Test answer is " << finalAns << "\n";
 
--- dubaitTest/DubaitTest/task1.cpp
+++ dubaitTest/DubaitTest/task1.cpp
@@ -1,87 +1,33 @@
 #include <iostream>
+#include <vector>
 
 
-
-
-unsigned long long  task1()
+int task1()
 {
-    unsigned long long counts[28] {0};
+    std::vector<int> F(100001, 0);
 
     // Из условия задачи
-    counts[0] = 1; // F[1] = 1
-    counts[1] = 2; // F[2] = 2
+    F[1] = 1;
+    F[2] = 2;
 
-    int maxNum = 100000;
-    // до 100000
-    int iter = 1;
-    // для сумм - будет считаться в обратном порядке от основного итератора
-    int sumIter = iter;
+    int target_index = 0;
 
-
-    while (iter != maxNum)
+    for (int n = 3; n <= 100000; ++n)
     {
-        // попробовать через указатели двигаться пачками по 26
-        // рассчет сумм
-        for (int i = 0; i < 28; ++i)
+        for (int i = 0; i < 13; ++i)
         {
-            // рассчет для нечетного числа
-            // исключение самого первого числа из рассчета
-            if ((iter % 2 != 0) & (iter != 1))
+            // число с шагом
+            target_index = n - 1 - (i * 2);
+            if (target_index < 1)
             {
-                for (int j = i; j >= 0; --j)
-                {
-                    if (sumIter % 2 == 0)
-                    {
-                        counts[i] += counts[j];
-                    }
-                    --sumIter;
-                }
-            }
-
-            // рассчет для четного числа
-            // исключение второго числа из рассчета
-            if ((iter % 2 == 0) & (iter != 2))
-            {
-                for (int j = i; j >= 0; --j)
-                {
-                    if (sumIter % 2 != 0)
-                    {
-                        counts[i] += counts[j];
-                    }
-                    // двигаемся назад по списку чисел
-                    --sumIter;
-                }
-            }
-
-            if (iter == maxNum)
-            {
-
                 break;
+            } else
+            {
+                // запись последних 5 чисел (обход переполнения)
+                F[n] = (F[n] + F[target_index]) % 100000;
             }
-            // переход к следующему числу
-            ++iter;
-            sumIter = iter;
         }
     }
 
-
-
-
-
-// нужно чтобы в цикле было 13 чисел, из 25 получается по 12
-// либо внутри цикла менять количество условий и итераций
-// либо думать над новым условием цикла
-
-    // вывод для проверки
-    std::cout << "ПРОВЕРКА\n";
-    for (int i = 0; i < 28; ++i)
-    {
-        std::cout << counts[i] << "\n";
-    }
-
-    std::cout << iter << "\n";
-    std::cout << counts[27] << "\n";
-    std::cout << counts[28] << "\n";
-
-    return 1;
+    return F[100000];
 }
